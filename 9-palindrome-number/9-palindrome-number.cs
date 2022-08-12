174b68923112621be9b6c972acf6541d7a48c8ee
Stepanov Oleg <chelninec1@gmail.com>
2022-08-13 01:21:17 +0300

    Time: 110 ms (6.68%), Space: 30.2 MB (9.56%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cs
+++ 9-palindrome-number/9-palindrome-number.cs
@@ -1,5 +1,15 @@
 public class Solution {
     public bool IsPalindrome(int x) {
+        if(x<0 || (x%10==0 && x!=0))
+        {
+            return false;
+        }
+        
+        if(x>=0 && x<10)
+        {
+            return true;
+        }
+        
         var strX = x.ToString();
         var xA1 = strX.ToArray();
         var xA2 = xA1.Reverse().ToArray();
